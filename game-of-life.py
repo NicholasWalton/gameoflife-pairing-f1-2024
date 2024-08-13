7dba13fdcc3e782de366345522ad2041537d45e9
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-13 12:10:14 -0700

    clean up and refactor

--- game-of-life.py
+++ game-of-life.py
@@ -2,18 +2,11 @@ import copy
 import os
 import random
 import time
-from pprint import pprint
 
 
 def main():
     grid = [random_row() for _ in range(15)]
-
-    # grid[1][1] = 1
-    # grid[1][2] = 1
-    # grid[1][3] = 1
-    # print()
-    # neighbors(grid)
-    # print()
+    
     generation_count = 0
     while True:
         os.system("cls")
@@ -29,11 +22,24 @@ def random_row():
     return [random.randint(0, 1) for _ in range(60)]
 
 
-def neighbors(grid):
+def next_generation(grid):
+    new_grid = copy.deepcopy(grid)
     for i, _ in enumerate(grid):
         for j, _ in enumerate(grid[i]):
-            print(count_neighbors(grid, i, j), end="")
-        print()
+            new_grid[i][j] = update_cell(grid, i, j)
+
+    return new_grid
+
+def update_cell(grid, i, j):
+    neighbors = count_neighbors(grid, i, j)
+
+    if grid[i][j] == 1:
+        if neighbors < 2 or neighbors > 3:
+            return 0
+    else:
+        if neighbors == 3:
+            return 1
+    return grid[i][j]
 
 
 def count_neighbors(grid, row, col):
@@ -46,21 +52,6 @@ def count_neighbors(grid, row, col):
                 count += 1
     return count
 
-
-def next_generation(grid):
-    new_grid = copy.deepcopy(grid)
-    for i, _ in enumerate(grid):
-        for j, _ in enumerate(grid[i]):
-            neighbors = count_neighbors(grid, i, j)
-
-            if grid[i][j] == 1:
-                if neighbors < 2 or neighbors > 3:
-                    new_grid[i][j] = 0
-            else:
-                if neighbors == 3:
-                    new_grid[i][j] = 1
-
-    return new_grid
 
 if __name__ == "__main__":
     main()
